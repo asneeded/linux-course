8681ffcd12821786b7df8cf7b14ce46abb57ff3e
root <root@DESKTOP-7S09KKI.localdomain>
2020-09-03 15:35:58 -0400

    install nfs-server and nfs-utils

--- server_setup.sh
+++ server_setup.sh
@@ -22,30 +22,36 @@ share='/mnt/server-share'
 # Update System
 if ping -c 2 8.8.8.8;then
     yum update && yum upgrade -y
+else
+    echo "No network connection, can not update"
+    exit 3
 fi
 
 
 # Setup student account
 if ! grep $account /etc/passwd ;then
     useradd -G wheel -p '$6$eNzUajbJpHs5RNd0$7RcTgCAhOZNEaJn8YxGIW9fo02LJHzmtqW9HEgcUW.ZvApZQA/IGrUA6SeMtlw4ajKVnfXPDMepyS/Qm9NZg50' $account
+    echo "Created the $account account and added to wheel group"
 fi
 
 # Setup NFS
+yum install nfs-server nfs-utils
 if [[ -f /etc/exports ]];then
     printf "Setting up NFS\n"
     if [[ ! -d $share ]];then
         mkdir -p $share
     fi
     if grep $share /etc/exports; then
-        echo ''
+        echo '/etc/exports already configured'
     else
+        yum install nfs-server
         printf "$share *(rw,sync) #Server share\n" >> /etc/exports
         exportfs -ra
         sysemtctl disable --now firewalld
         iptables -I INPUT -p tcp -m multiport --dport 111,2049,20048 -m comment --comment "NFS/mountd Ports TCP" -j ACCEPT
         iptables -I INPUT -p udp -m multiport --dport 111,2049,20048 -m comment --comment 'NFS/mountd Ports UDP' -j ACCEPT
-        systemctl enable --now nfs
-        sysemtctl restart nfs
+        systemctl enable --now nfs-server
+        sysemtctl restart nfs-server
     if ping -c 2 8.8.8.8 ;then
         yum install wget -y 
         wget -O ${share}/linux-cheatsheet00.pdf http://images.linoxide.com/linux-cheat-sheet.pdf
